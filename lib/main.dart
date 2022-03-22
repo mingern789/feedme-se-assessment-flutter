61d07b08ab48133945eea128963f2a989a4385f0
mingern789 <63950500+mingern789@users.noreply.github.com>
2022-03-22 11:01:41 +0800

    code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Mcdonalds Bot Kitchen Simulator'),
@@ -32,15 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -84,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return false;
   }
 
-  addCompletedOrder(order) {
+  void addCompletedOrder(order) {
     setState(() {
       completedOrdersList.add(order);
       checkForPending();
@@ -92,8 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  completeOrder(order) {
-    // const completeStore = useCompleteOrdersStore();
+  void completeOrder(order) {
     setState(() {
       addCompletedOrder(order);
       pendingOrdersList.removeWhere((item) => item['name'] == order);
@@ -102,7 +82,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   markAsComplete(id, order) {
-    // const orderStore = usePendingOrdersStore();
     setState(() {
       for (var i in botList) {
         /** Matches available bots to assigned bot order id for completion, will only run when found,
@@ -116,7 +95,6 @@ class _MyHomePageState extends State<MyHomePage> {
           for (var e in pendingOrdersList) {
             if (e['name'] == order['name']) {
               completeOrder(e['name']);
-              // orderStore.completeOrder(e.name);
             }
           }
           return;
@@ -128,7 +106,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void getToWork(order) {
-    // const orderStore = usePendingOrdersStore();
     var botId;
     setState(() {
       if (botsAvailable()) {
@@ -142,7 +119,6 @@ class _MyHomePageState extends State<MyHomePage> {
         //Will mark the ongoing order as completed once 10s mark is passed
         var timer = Timer(
             const Duration(seconds: 10), () => markAsComplete(botId, order));
-        // const timeout = setTimeout(this.markAsComplete, 10000, botId, order);
 
         //Put the bot in working mode
         for (var i in botList) {
@@ -161,11 +137,9 @@ class _MyHomePageState extends State<MyHomePage> {
         updateUI();
       }
     });
-    //searches for any afk bots to get them to do any orders on waiting
   }
 
-  checkForPending() {
-    // const orderStore = usePendingOrdersStore();
+  void checkForPending() {
     for (var i in pendingOrdersList) {
       //checks for any order on waiting and also whether there are any bots available to handle the order
       if (i['status'] == "waiting" && botsAvailable()) {
@@ -173,12 +147,6 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     }
   }
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     return;
-  //   });
-  // }
 
   Widget pendingOrders() {
     return Column(
@@ -226,11 +194,9 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {
         botList.add({'botStatus': "AFK", 'id': botId});
         botId++;
-        stringBotList =
-            botList.isNotEmpty ? botList.join(', ') : 'No bots added';
+        updateUI();
       });
       checkForPending();
-      //checkForPending(); //makes newly added bot to check for any orders on wait
     }
 
     bool botDeleted(botId) {
@@ -337,9 +303,7 @@ class _MyHomePageState extends State<MyHomePage> {
             'assignedTo': null
           });
           vipOrderId++;
-          stringPendingList = pendingOrdersList.isNotEmpty
-              ? pendingOrdersList.join(', ')
-              : 'No pending orders';
+          updateUI();
         });
       } else {
         setState(() {
@@ -349,9 +313,7 @@ class _MyHomePageState extends State<MyHomePage> {
             'assignedTo': null
           });
           vipOrderId++;
-          stringPendingList = pendingOrdersList.isNotEmpty
-              ? pendingOrdersList.join(', ')
-              : 'No pending orders';
+          updateUI();
         });
       }
       checkForPending();
@@ -395,37 +357,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             pendingOrders(),
             completedOrders(),
@@ -434,11 +372,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
     );
   }
 }
